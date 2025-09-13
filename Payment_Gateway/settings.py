25ea6f56bd2c317be6fcc6173ab77b8868c436a0
6jp9 <jayprakash1405401@gmail.com>
2025-09-13 23:45:03 +0530

    products added

--- Payment_Gateway/settings.py
+++ Payment_Gateway/settings.py
@@ -17,6 +17,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 TEMPLATES_DIR = BASE_DIR/'templates'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
--- home/views.py
+++ home/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import ProductForms
-
+from user.models import Merchants
+from .models import Products
 # Create your views here.
 def home(request):
-    return render(request,'home/home.html')
+    Product = Products.objects.all()
+    return render(request,'home/home.html',{'product':Product})
 def orders(request):
     return render(request,'user/my_orders.html')
-def add_product(request):
-    form = ProductForms()
+def add_product(request,username):
+    if request.method=='POST':
+        merchant = Merchants.objects.get(username=username)
+        form = ProductForms(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.merchant_id = merchant.merchant_id
+            product.save()
+            return redirect(f'/merchant_profile/{merchant.username}/')
+    else:
+        form = ProductForms()
     return render(request,'home/addproductform.html',{'form':form})
