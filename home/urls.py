269902d97029d859eb0bba42fd071df412eeea92
6jp9 <jayprakash1405401@gmail.com>
2025-09-15 22:59:45 +0530

    products added through API

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('orders/',views.orders,name='orders'),
-    path('addproduct/<str:username>/',views.add_product)
+    path('addproduct/<str:username>/',views.add_product),
+    path('product/<int:product_id>',views.product_dtls),
+    path('api_product/<int:id>',views.api_product_dtls),
 ]
--- home/views.py
+++ home/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render,redirect
 from .forms import ProductForms
 from user.models import Merchants
 from .models import Products
+import requests
 # Create your views here.
 def home(request):
     Product = Products.objects.all()
-    return render(request,'home/home.html',{'product':Product})
+    url = f"https://fakestoreapi.com/products/"
+    response = requests.get(url)
+    api_product = response.json()
+    return render(request,'home/home.html',{'product':Product,'api':api_product})
 def orders(request):
-    return render(request,'user/my_orders.html')
+    return render(request,'home/my_orders.html')
 def add_product(request,username):
     if request.method=='POST':
         merchant = Merchants.objects.get(username=username)
@@ -20,3 +24,13 @@ def add_product(request,username):
     else:
         form = ProductForms()
     return render(request,'home/addproductform.html',{'form':form})
+
+def product_dtls(request,product_id):
+    product = Products.objects.get(product_id=product_id)
+    return render(request,'home/product.html',{'product':product})
+
+def api_product_dtls(request,id):
+    url = f'https://fakestoreapi.com/products/{id}'
+    response = requests.get(url)
+    item = response.json()
+    return render(request,'home/product.html',{'item':item})
