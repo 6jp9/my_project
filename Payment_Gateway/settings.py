127f8a53d53d29ae8e2d9c243f79af5d8c6bf4c1
6jp9 <jayprakash1405401@gmail.com>
2025-09-29 20:13:14 +0530

    stripe test payment api initiated

--- Payment_Gateway/settings.py
+++ Payment_Gateway/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,5 +145,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jayaprakash1405401@gmail.com'
-EMAIL_HOST_PASSWORD = '#'  # Use App Password, NOT your Gmail password
+EMAIL_HOST_USER = config('EMAIL')
+EMAIL_HOST_PASSWORD = config("APP_PASSWORD")  # Use App Password, NOT your Gmail password
+
+#Stripe API keys
+STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY")
+STRIPE_PUBLISHABLE_KEY = config("STRIPE_PUBLISHABLE_KEY")
